452b1e79613ce07eabee31ac8788f0169ac4b920
akash <akash300383@gmail.com>
2020-08-24 18:32:38 +0530

    Day 1 Notes

--- New folder/CarTest.java
+++ New folder/CarTest.java
@@ -1,27 +1,25 @@
 class Car {
-	int wheel;
-	String color;
-	float price;
-	void carInfo(String name) {
-		System.out.println("Car Info "+name);
-		System.out.println("Wheel "+wheel);
-		System.out.println("Color "+color);	
-		System.out.println("Price "+price);
-			
+
+	Car() {
+		System.out.println("Object created....");
+	}
+		
+	void carInfo() {
+		System.out.println("Car class method");	
 	}
 }
 class CarTest {
 	public static void main(String args[]) {
-	Car innova  = new Car();
-	innova.wheel = 4;
-	innova.color = "Gray";
-	innova.price = 1500000;
-	innova.carInfo("Innova");
-	
-	Car swift  = new Car();
-	swift.wheel = 4;
-	swift.color = "Black";
-	swift.price = 850000;
-	swift.carInfo("Swift");	
+	Car obj1 = new Car();
+	obj1.carInfo();	
+	obj1.carInfo();
+	obj1.carInfo();
+
+	Car obj2 = new Car();
+	obj2.carInfo();
+	obj2.carInfo();
 	}
 }
+
+
+
---
+++ New folder/Test.java
@@ -0,0 +1,34 @@
+interface A {
+	int add(int x, int y);
+}
+
+interface B {
+	int sub(int x, int y);
+}
+
+
+class Server implements A,B{
+	public int add(int x, int y) {
+		return x+y;
+	}
+	public int sub(int x, int y) {
+		return x+y;
+	}
+	void ownMethod() {
+		System.out.println("Server own method");
+	}
+}
+class Test {
+	public static void main(String args[]) {
+	Server ss = new Server();
+	A obj1 = new Server();
+	B obj2 = new Server();	
+	System.out.println(obj1.add(1,2));		
+	//System.out.println(obj1.sub(1,2));		
+	//System.out.println(obj2.add(1,2));
+	System.out.println(obj2.sub(1,2));				
+	}
+}
+
+
+
